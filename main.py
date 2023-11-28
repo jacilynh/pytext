7e007136604b16ee19f49f0fb5341809e353fac1
jacilynh <jacihayden@gmail.com>
2023-11-27 23:26:21 -0800

    Search for phrases instead of only single words

--- main.py
+++ main.py
@@ -3,44 +3,30 @@ import re
 import string
 from collections import defaultdict
 
-def tokenize(text):
-    # Split text into words, remove punctuation, and lower case
-    return re.findall(r'\b\w+\b', text.lower())
+def get_context(text, term, window=1000):
+    # This function finds the term in the text and returns the context around it
+    for match in re.finditer(re.escape(term), text, re.IGNORECASE):
+        start = max(match.start() - window, 0)
+        end = min(match.end() + window, len(text))
+        yield text[start:end]
 
-def create_index(root_directory):
-    index = defaultdict(set)
+def search_and_context(query, root_directory):
+    results = defaultdict(list)
+
     for directory, subdirs, files in os.walk(root_directory):
         for filename in files:
             if filename.endswith(".txt"):
                 filepath = os.path.join(directory, filename)
                 with open(filepath, 'r', encoding='utf-8') as file:
-                    for word in tokenize(file.read()):
-                        index[word].add(filepath)  # Store the full path
-    return index
-
-def get_context(text, term, window=1000):
-    # This function finds the term in the text and returns the context around it
-    for match in re.finditer(term, text):
-        start = max(match.start() - window, 0)
-        end = min(match.end() + window, len(text))
-        yield text[start:end]
+                    content = file.read()
+                    for context in get_context(content, query):
+                        results[filepath].append(context)
+    return results
 
 def highlight_term(text, term):
     # Markdown bold format
     highlighted_text = text.replace(term, f"**{term}**")
     return highlighted_text
-
-def search_and_context(query, root_directory, index):
-    results = defaultdict(list)
-    query_lower = query.lower()  # Convert query to lowercase
-
-    # Directly use file paths from the index
-    for filepath in index.get(query_lower, set()):
-        with open(filepath, 'r', encoding='utf-8') as file:
-            content = file.read().lower()  # Convert content to lowercase
-            for context in get_context(content, re.escape(query_lower)):
-                results[filepath].append(context)
-    return results
 
 def sanitize_filename(filename):
     """Create a valid filename from a string."""
@@ -50,12 +36,12 @@ def sanitize_filename(filename):
 
 # Usage Example
 script_dir = os.path.dirname(os.path.realpath(__file__)) # Get the directory of the current script
-# Relative path to the data directory from the script
 data_dir = os.path.join(script_dir, 'data')  # 'data' is a folder in the same directory as your script
-index = create_index(data_dir)
-query = "bridge"  # Replace with your search term
 
-results = search_and_context(query, data_dir, index)
+# Prompt the user for a search term
+query = input("Enter the search term: ")
+
+results = search_and_context(query, data_dir)
 
 # Create a Markdown file name from the search term
 file_name = sanitize_filename(query) + ".md"
@@ -66,7 +52,7 @@ with open(file_path, 'w', encoding='utf-8') as file:
     for filename, contexts in results.items():
         file.write(f"### In file '{filename}':\n")
         for context in contexts:
-            highlighted_context = highlight_term(context, query.lower())
+            highlighted_context = highlight_term(context, query)
             file.write(highlighted_context + "\n\n---\n\n")
 
 print(f"Search results saved in Markdown format to {file_path}")
